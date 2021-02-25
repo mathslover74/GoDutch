4f5a3dfa7e3f376a4bebda6e1c02f37429ace59e
mathslover74 <Jason.chng.sy@gmail.com>
2021-02-25 23:26:53 +0800

    login page with authentication done. working on logout page to fix into nav.

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const methodOverride = require('method-override');
 // const mongoURI = process.env.DB_URI || 'mongodb://localhost:27017/';
 const mongoose = require('mongoose');
+const session = require('express-session');
 require('dotenv').config();
+const app = express();
+const userController = require('./controllers/users.js');
+const sessionsController = require('./controllers/sessions.js');
+
+
 
 // Configuration
 const PORT = process.env.PORT|| 8080;
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/godutch'
-
-
-const app = express();
 
 // Middleware
 // allows us to use put and delete methods
@@ -19,16 +22,37 @@ app.use(express.urlencoded({ extended: false }));
 //allow to access to public
 app.use(express.static('public'));
 
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
 // Database
 mongoose.connect(mongoURI, { useNewUrlParser: true })
 mongoose.connection.once('open', () => {
   console.log('connected to mongo');
 });
 
+app.use('/users', userController);
+app.use('/sessions', sessionsController);
+
+
 // Routes
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index.ejs', {
+    currentUser: req.session.currentUser
+  });
+});
+
+app.get('/app', (req, res)=>{
+  if(req.session.currentUser){
+      res.render('app/index.ejs')
+  } else {
+      res.redirect('/sessions/new');
+  }
 })
+
 
 
 // Listen

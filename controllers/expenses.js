05407afc344a9d242d6cc13c3dbaa11e9fec1e42
mathslover74 <Jason.chng.sy@gmail.com>
2021-03-02 15:28:06 +0800

    resolved authentucated change model date to string change inor css and logo

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -16,7 +16,7 @@ const isAuthenticated = (req, res, next) => {
   if (req.session.currentUser) {
     return next();
   } else {
-    res.redirect('/');
+    res.redirect('/sessions/new');
   }
 }
 
@@ -31,32 +31,34 @@ const isAuthenticated = (req, res, next) => {
 //   });  
 // });
 
-expenses.get('/dashboard', (req, res)=>{
-  if(req.session.currentUser){
+expenses.get('/dashboard',isAuthenticated, (req, res)=>{
+
+  // if(req.session.currentUser){
       expensesLog.find({}, (error,allExpenses )=>{
         res.render('expenses/index.ejs', {
             expenses: allExpenses
         });
     });
-  } else {
-      res.redirect('/sessions/new');
-  }
+  // } else {
+  //     // res.redirect('/sessions/new');
+  //     res.redirect('/');
+  // }
 });
 
 
 ///Create create route
-expenses.get('/new', (req, res) => {
+expenses.get('/new',isAuthenticated , (req, res) => {
   // res.render('expenses/new.ejs');
-  if (req.session.currentUser) {
+  // if (req.session.currentUser) {
     res.render('expenses/new.ejs');
-  } else {
-    res.redirect('/sessions/new');
-  }
+  // } else {
+  //   res.redirect('/sessions/new');
+  // }
   // res.send('linked');
 });
 
 ///Create route create data in mongoDB
-expenses.post('/', (req, res)=>{
+expenses.post('/',isAuthenticated , (req, res)=>{
   expensesLog.create(req.body, (err, createItem) => {
     // res.send(req.body);
     res.redirect('/expenses/dashboard');
@@ -73,29 +75,51 @@ expenses.post('/', (req, res)=>{
 //   });
 // });
 
-expenses.get('/seed', (req, res)=>{
+expenses.get('/seed',isAuthenticated, (req, res)=>{
   expensesLog.create([
     {
-      date: '2021-02-11',
+      createdBy: 'Mochi',
+      date: '20-01-2021',
       description: 'drinks',
       name:'friend 1',
       paidByYou: true,
       amount: 5
-    }, {
-      date: '2021-02-11',
+    }, 
+    
+    {
+      createdBy: 'Mochi',
+      date: '25-01-2021',
       description: 'Food',
       name:'friend 2',
+      paidByYou: true,
+      amount: 15
+    },
+
+    {
+      createdBy: 'Sushi',
+      date: '29-01-2021',
+      description: 'Coke',
+      name:'friend 2',
+      paidByYou: true,
+      amount: 16
+    },
+
+    {
+      createdBy: 'Cherry',
+      date: '4-01-2021',
+      description: 'Cake',
+      name:'friend 1',
       paidByYou: true,
       amount: 10
     },
   ], (err, data)=>{
-      res.redirect('/expenses');
+      res.redirect('/expenses/dashboard');
   })
 });
 
 ///Create Show Route
 
-expenses.get('/:id', (req, res)=>{
+expenses.get('/:id' ,isAuthenticated , (req, res)=>{
   expensesLog.findById(req.params.id, (err, foundItems) => {
       res.render('expenses/show.ejs', {
           expenses:foundItems
@@ -106,17 +130,17 @@ expenses.get('/:id', (req, res)=>{
 });
 
 ///Create Delete route
-expenses.delete('/:id', (req,res) => {
+expenses.delete('/:id',isAuthenticated , (req,res) => {
   // res.send('delete');
   expensesLog.findByIdAndRemove(req.params.id, (err,data) => {
-    res.redirect('/expenses'); ///redirect back to index
+    res.redirect('/expenses/dashboard'); ///redirect back to index
   })
 });
 
 //Create edit route/Page
 
 //get edit
-expenses.get('/:id/edit', (req, res)=>{
+expenses.get('/:id/edit',isAuthenticated , (req, res)=>{
   expensesLog.findById(req.params.id, (err, foundItem)=>{ //find the item
       res.render('expenses/edit.ejs', {
         expenses: foundItem //pass in found item
@@ -124,9 +148,8 @@ expenses.get('/:id/edit', (req, res)=>{
   }); 
 });
 
-
-///put edit
-expenses.put('/:id', (req, res) => {
+///put edit and update route
+expenses.put('/:id' ,isAuthenticated, (req, res) => {
 	if (req.body.paidByYou === 'on') {
 		req.body.paidByYou = true;
 	} else {
@@ -138,24 +161,23 @@ expenses.put('/:id', (req, res) => {
 		{ new: true },
 		(err, updatedModel) => {
 			res.redirect('/expenses/'+req.params.id);
-		}
-	);
+		});
 });
 
 // Create route
-expenses.post('/:id', (req, res) => {
+expenses.post('/:id' ,isAuthenticated, (req, res) => {
 	if (req.body.paidByYou === 'on') {
-		//if checked, req.body.readyToEat is set to 'on'
+		//if checked, req.body.paidByYou is set to 'on'
 		req.body.paidByYou = true;
 	} else {
-		//if not checked, req.body.readyToEat is undefined
+		//if not checked, req.body.paidByYou is undefined
 		req.body.paidByYou = false;
 	}
 
-	expensesLog.create(req.body, (err, createdItem) => {
+	expensesLog.create(req.body ,isAuthenticated, (err, updatedModel) => {
 		if (err) res.send(err.message);
 
-		res.redirect('/expenses'+req.params.id);
+		res.redirect('/expenses/'+req.params.id);
 	});
 });
 
--- models/expenses.js
+++ models/expenses.js
@@ -1,9 +1,13 @@
 // const { static } = require('express')
+const { text } = require('express');
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const expensesSchema = new Schema({
-  date: { type:Date, required: true},
+  // date: { type:Date, required: true},
+  // createdBy: {type: Schema.Types.ObjectId, ref:'username'},
+  createdBy: {type: String, required: true},
+  date: { type: String, required: true},
   description:  { type: String, required: true },
   name:  { type: String, required: true },
   paidByYou:  { type: Boolean },
